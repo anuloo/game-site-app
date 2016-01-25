b941ab337d968d29dc7637ece48d57ae4b8f140f
anuloo <jollzy@gmail.com>
2016-01-25 01:46:08 +0000

    changing port to 4000 , conected to mongo db hooked it up with the api

--- app/server/bin/server.js
+++ app/server/bin/server.js
@@ -1,6 +1,6 @@
 var app = require('../app');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('server listening on port ' + server.address().port);
